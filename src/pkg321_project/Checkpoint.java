d799ad3f18f891f05cbea17f0a90a2024d0c5285
Sierra Elisa Laney <sierralaney@gmail.com>
2020-11-10 17:59:41 -0600

    Current Status
    Binary Tree built. GUI building. Checkpoint needs to be coded.

---
+++ src/pkg321_project/Checkpoint.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg321_project;
+
+/**
+ *
+ * @author Sierra Laney
+ */
+public class Checkpoint {
+boolean checkpoint = false; 
+
+public void continue_game(Node n) {
+    
+}
+    
+}
---
+++ src/pkg321_project/Node.java
@@ -0,0 +1,18 @@
+
+package pkg321_project;
+
+public class Node {
+      int data;
+     String story;
+     Node left;
+     Node right; //left and right children
+     
+    //constructor
+        public  Node(int t_data, String t_story){
+            data=t_data; 
+            left=null;
+            right=null;
+            story=t_story;
+      }
+}
+
